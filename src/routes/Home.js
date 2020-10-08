8806144d647034310b33e7a8927a073a019f0cc9
김준섭 <wnstjq616@gmail.com>
2020-10-08 19:20:45 +0900

    first query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,21 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  query {
+    movies(limit: 5) {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+  return `<h1>${data.movies[0].title}</h1>`;
+};
