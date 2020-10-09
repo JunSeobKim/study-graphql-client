226daa2d263fcda8360170e87f553297ece0f086
김준섭 <wnstjq616@gmail.com>
2020-10-09 21:17:29 +0900

    css use styled-components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIES = gql`
   query {
@@ -11,11 +12,49 @@ const GET_MOVIES = gql`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   console.log(data);
-  return `<h1>${data.movies[0].title}</h1>`;
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo</Title>
+        <Subtitle>GraphQL</Subtitle>
+      </Header>
+    </Container>
+  );
 };
